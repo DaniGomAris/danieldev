187b3881d6a280f8916af691bd3f3b7509395e6b
DanielDev <danielgomezaristizabal2020@gmail.com>
2026-02-08 01:02:11 -0500

    completed header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Empty } from './shared/empty/empty';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: Empty },
+  { path: 'about', component: Empty },
+  { path: 'projects', component: Empty },
+  { path: 'blog', component: Empty },
+  { path: 'certifications', component: Empty },
+  { path: 'contact', component: Empty },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,27 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { Header } from "./layout/header/header";
+
+import { Home } from './sections/home/home';
+import { About } from './sections/about/about';
+import { Projects } from './sections/projects/projects';
+import { Certifications } from './sections/certifications/certifications';
+import { Contact } from './sections/contact/contact';
+import { Blog } from './sections/blog/blog';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    Header, 
+    Home, 
+    About, 
+    Projects,
+    Blog, 
+    Certifications, 
+    Contact
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
